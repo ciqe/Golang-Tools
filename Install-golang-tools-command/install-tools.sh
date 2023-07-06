df352c44a372f046bb4e12ce51ada028cb7516e8
Name less User <106626236+ciqe@users.noreply.github.com>
2023-07-06 18:43:02 +0600

    Create install-tools.sh

---
+++ Install-golang-tools-command/install-tools.sh
@@ -0,0 +1,50 @@
+echo -e "\n\n\n\n\nInstalling assetfinder..."
+go install -v github.com/tomnomnom/assetfinder@latest
+
+echo -e "\n\n\n\n\nInstalling anew..."
+go install -v github.com/tomnomnom/anew@latest
+
+echo -e "\n\n\n\n\nInstalling dnsx..."
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+
+echo -e "\n\n\n\n\nInstalling ffuf..."
+go install -v github.com/ffuf/ffuf@latest
+
+echo -e "\n\n\n\n\nInstalling gau..."
+go install -v github.com/lc/gau/v2/cmd/gau@latest
+
+echo -e "\n\n\n\n\nInstalling gf..."
+go install -v github.com/tomnomnom/gf@latest
+
+echo -e "\n\n\n\n\nInstalling httprobe..."
+go install -v github.com/tomnomnom/httprobe@latest
+
+echo -e "\n\n\n\n\nInstalling httpx..."
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+echo -e "\n\n\n\n\nInstalling katana..."
+go install -v github.com/projectdiscovery/katana/cmd/katana@latest
+
+echo -e "\n\n\n\n\nInstalling naabu..."
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+
+echo -e "\n\n\n\n\nInstalling nuclei..."
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+echo -e "\n\n\n\n\nInstalling puredns..."
+go install -v github.com/d3mondev/puredns/v2@latest
+
+echo -e "\n\n\n\n\nInstalling qsreplace..."
+go install -v github.com/tomnomnom/qsreplace@latest
+
+echo -e "\n\n\n\n\nInstalling subfinder..."
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+echo -e "\n\n\n\n\nInstalling subjack..."
+go install -v github.com/haccer/subjack@latest
+
+echo -e "\n\n\n\n\nInstalling uncover..."
+go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest
+
+echo -e "\n\n\n\n\nInstalling waybackurls..."
+go install -v github.com/tomnomnom/waybackurls@latest
